340599e25915562c264755ea8f0c738b19b23391
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2015-07-20 15:31:20 -0700

    modify

--- TCGAscripts/seg2matrix/getRefGene_hg19.sh
+++ TCGAscripts/seg2matrix/getRefGene_hg19.sh
@@ -2,4 +2,4 @@
 
 curl -O http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz
 gunzip -c refGene.txt.gz > refGene_hg19
-echo '{ "cgdata" : {"name" : "refGene_hg18", "type" : "genePred", "assembly" : "hg19"} }' > refGene_hg19.json
+echo '{ "name" : "refGene_hg19", "type" : "genePredExt", "assembly" : "hg19", "url":"http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz"}' > refGene_hg19.json
