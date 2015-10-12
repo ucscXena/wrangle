80952b4d9d0724bbd403241a02687104346110f8
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2015-10-12 11:22:11 -0700

    remove seg2matrix

--- TCGAscripts/seg2matrix/getRefGene_hg18.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -O http://hgdownload.cse.ucsc.edu/goldenPath/hg18/database/refGene.txt.gz
-gunzip -c refGene.txt.gz > refGene_hg18
-echo '{ "cgdata" : {"name" : "refGene_hg18", "type" : "genePred", "assembly" : "hg18"} }' > refGene_hg18.json
