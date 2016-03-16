b93e337d519e9e78f8df264a7742e63cc9991fef
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-03-16 10:18:30 -0700

    add

---
+++ PCAWGscripts/download_list.py
@@ -0,0 +1,36 @@
+import string,os,sys
+
+freezefile = "release_mar2016.v1.txt"
+keyword ="_variant_calling_repo"
+
+def parseFreezeFile (infile):
+    fin =open (infile,'r')
+    reg ={}
+    
+    #header
+    data = string.split(fin.readline(),'\t')
+    N = len(data)
+    for i in range (0, N):
+        if string.find(data[i],keyword)!=-1:
+            method = string.replace(data[i],keyword,"")
+            if not os.path.exists("PCAWG/"+method):
+                os.system("mkdir PCAWG/"+ method)
+            reg[i]=open(method+"_list",'w')
+            
+
+    #datalines
+    for line in fin.readlines():
+        data = string.split(line,'\t')
+        n =len(data)
+        for i in range (0, N):
+            if i in reg:
+                if string.strip(data[i])!="":
+                    fout = reg[i]
+                    fout.write(data[i]+'\t'+data[i+1]+'\n')
+
+    fin.close()
+    for i in reg:
+        fout= reg[i]
+        fout.close()
+
+parseFreezeFile(freezefile)
