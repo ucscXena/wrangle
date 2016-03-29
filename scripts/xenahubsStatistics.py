baf9aec12e99599508a55ef473ab36049f2651c2
Jing <jingchunzhu@gmail.com>
2016-03-29 10:13:40 -0700

    add xenaquery

---
+++ scripts/xenahubsStatistics.py
@@ -0,0 +1,44 @@
+import xena_query as xena
+
+def uniqSamples(huburl):
+  allSamples ={}
+  allCohorts = xena.all_cohorts(huburl)
+
+  for cohort in allCohorts:
+    samples = xena.all_samples(huburl, cohort)
+    for sample in samples:
+      if sample not in allSamples:
+        allSamples[sample]=""
+  return allSamples
+
+def totalSamples(hubs):
+  totalSamples={}
+  for hub in hubs:
+    samples = uniqSamples(hub)
+    print hub, "samples", len(samples)
+    for sample in samples:
+      if sample not in totalSamples:
+        totalSamples[sample]=""
+  print "total samples:", len(totalSamples)
+
+
+def totalDatasets(hubs):
+  totalDatasets =[]
+  for hub in hubs:
+    totalDatasets.extend(xena.datasets_list(hub))
+  print "total datasets:", len(totalDatasets)
+
+
+hubs = ["https://genome-cancer.ucsc.edu/proj/public/xena",
+  "https://tcga.xenahubs.net",
+  "https://icgc.xenahubs.net",
+  "https://toil.xenahubs.net"
+]
+totalSamples(hubs)
+
+hubs = ["https://genome-cancer.ucsc.edu/proj/public/xena",
+  #"https://tcga.xenahubs.net",
+  #"https://icgc.xenahubs.net",
+  "https://toil.xenahubs.net"
+]
+totalDatasets(hubs)
