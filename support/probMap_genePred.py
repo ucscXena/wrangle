a3bff403d461852b0d469f1591dc94627037847c
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-05-13 00:36:49 -0700

    edit

--- support/probMap_genePred.py
+++ support/probMap_genePred.py
@@ -1,6 +1,7 @@
 import string,sys
 
-#return a dictionary of  hugo:[{(id,)(chr,), (start,), (end,)},]
+#return a dictionary of  {chrom:
+#                                {hugo:[{(id,),(chr,), (strand,),(start,), (end,)},],}}
 def parseGenePredToGene(fin):
     chromDic={}
     for line in fin.readlines():
@@ -32,7 +33,31 @@ def parseGenePredToGene(fin):
     return chromDic
 
 
+#id	gene	chrom	chromStart	chromEnd	strand
+#ENSG00000223972.5	DDX11L1	chr1	11869	14409	+
+
 def parseProbeMapToGene(fin):
     fin.readline()
-    return parseGenePredToGene(fin)
+    dic={}
+    for line in fin.readlines():
+        data = string.split(line[:-1],'\t')
+        id =data[0]
+        chr=data[2]
+        strand =data[5]
+        start = data[3]
+        end =data[4]
+        hugo =data[1]
+        item ={}
+        item['id']=id
+        item['chr']=chr
+        item['strand']=strand
+        item['start']= float(start)
+        item['end']=float(end)
+        item['hugo']= string.split(hugo,','):
 
+        if id not in dic:
+            dic[id]=[]
+            dic[id]=item
+            
+    fin.close()
+    return dic
