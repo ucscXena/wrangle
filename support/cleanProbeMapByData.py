840568c1d5ba0c36be3bb4ad5f87ace12cca13ef
Jing <jingchunzhu@gmail.com>
2016-08-23 17:45:38 -0700

    edit

---
+++ support/cleanProbeMapByData.py
@@ -0,0 +1,39 @@
+import string, sys, os
+
+def cleanProbeMapByData (matrixfile, probeMapfile, outputfile):
+    fprobe = open(probeMapfile,'U')
+    fout = open(outputFile,'w')
+
+    #matrix features
+    os.system("cut -f 1 "+ matrixfile + " > features")
+    fin = open("features",'r')
+    features = {}
+    map(lambda x: features[x]='', fin.readlines())
+    fin.close()
+
+    #probeMap
+    line = fprobe.readline()
+    fout.write(line)
+
+    while 1:
+        line = fprobe.readline()
+        if line =="":
+            break
+
+        data = string.split(line[:-1],'\t')
+        if data[0] in features:
+           fout.write(line)
+
+    fprobe.close()
+    fout.close()
+
+if len(sys.argv[:])!=4:
+    print "python cleanProbeMapByData.py genomicMatrix probeMap newProbeMap"
+    print
+    sys.exit()
+
+matrixfile = sys.argv[1]
+probeMapfile = sys.argv[2]
+outputfile = sys.argv[3]
+
+cleanProbeMapByData (matrixfile, probeMapfile, outputfile)
