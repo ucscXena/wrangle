0dfaa6a55700d13f5143a5c4e6e68ce16b37088d
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-11-16 01:43:11 -0800

    add

---
+++ PCAWGscripts/parseCNV.py
@@ -0,0 +1,40 @@
+import string, sys, os
+dataCol = 3
+
+def writeHeader(fout, line):
+    fout.write("sample\tchr\tstart\tend\tvalue" +'\t')
+    data = string.split(line,'\t')
+    fout.write(string.join(data[3:],'\t'))
+    
+def process(inDir, output):
+    fout = open(output,'w')
+    count = 0
+    for file in os.listdir(inDir):
+        fname = inDir + file
+        sampleName = string.split(file,'.')[0]
+
+        fin = open(fname,'r')
+        if count == 0:
+            writeHeader(fout, fin.readline())
+        count = count + 1
+
+        for line in fin.readlines():
+            fout.write(sampleName +'\t')
+            data = string.split(line,'\t')
+            fout.write(string.join(data[0:3],'\t') +'\t')
+            fout.write(data[dataCol]+'\t')
+            fout.write(string.join(data[3:],'\t'))
+        fin.close()
+    fout.close()
+
+
+if len(sys.argv[:])!= 3:
+    print "python parseCNV.py dir output"
+    print
+    sys.exit()
+
+inDir = sys.argv[1]
+if inDir[-1]!= "/":
+    inDir = inDir +"/"
+output = sys.argv[2]
+process(inDir, output)
