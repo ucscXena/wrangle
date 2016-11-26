d1f88d42696c228a1cb3b8799ffc938b302fded5
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-11-26 14:30:48 -0800

    edit

--- ICGCscripts/snv_parse.bash
+++ ICGCscripts/snv_parse.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for file in simple_somatic_mutation.open.*tsv ; do
+cancer='*'
+#cancer='MELA-AU'
+
+for file in simple_somatic_mutation.open.$cancer.tsv ; do
     output=${file/.tsv/.donor.xena}
     tmpfile=${file/.tsv/.donor.tmp}
     echo $output
@@ -8,12 +11,10 @@ for file in simple_somatic_mutation.open.*tsv ; do
     sed 1d $file |cut -f 2,9,10,11,15,17,26,27,29,30 | sort |uniq > $tmpfile &&  python ~/cgDataJing/ICGCscripts/icgcSNVcleanup.py $tmpfile $output && rm -f $tmpfile &
 done
 
-for file in simple_somatic_mutation.open.*tsv ; do
+for file in simple_somatic_mutation.open.$cancer.tsv ; do
     output=${file/.tsv/.sp.xena}
     tmpfile=${file/.tsv/.sp.tmp}
     echo $output
     #sed 1d $file |cut -f 4,9,10,11,15,17,26,27,29,30  | grep -v intron_variant |grep -v downstream_gene_variant |grep -v intergenic_region |sort |uniq > $tmpfile &&  python ~/cgDataJing/ICGCscripts/icgcSNVcleanup.py $tmpfile $output && rm -f $tmpfile &
     sed 1d $file |cut -f 4,9,10,11,15,17,26,27,29,30  | sort |uniq > $tmpfile &&  python ~/cgDataJing/ICGCscripts/icgcSNVcleanup.py $tmpfile $output && rm -f $tmpfile &
 done
-
-
