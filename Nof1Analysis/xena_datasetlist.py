da5e471fbcdb18f59083f4a3779ea8acccbc6e88
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-12-13 23:39:14 -0800

    edit

--- Nof1Analysis/xena_datasetlist.py
+++ Nof1Analysis/xena_datasetlist.py
@@ -17,7 +17,7 @@ GTEX_breast = {
 }
 
 TCGA_BRCA_tumors = {
-    "file": "/inside/depot4/CCI/TCGA_BRCA_RSEM_hugo_tpm",
+    "file": "data/TCGA_BRCA_RSEM_hugo_tpm",
     "fileLabel":"TCGA_breasts",
     "hub" : "https://toil.xenahubs.net",
     "dataset" : "tcga_RSEM_gene_tpm",
@@ -28,7 +28,7 @@ TCGA_BRCA_tumors = {
 }
 
 TCGA_TNBC = {
-    "file": "/inside/depot4/CCI/TCGA_TNBC_RSEM_hugo_tpm",
+    "file": "data/TCGA_TNBC_RSEM_hugo_tpm",
     "fileLabel":"TNBC",
     "hub" : "https://toil.xenahubs.net",
     "dataset" : "tcga_RSEM_gene_tpm",
