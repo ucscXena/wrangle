7a548f34b4abc18b9111848b84b993663372365e
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-12-31 23:34:48 -0800

    edit

--- support/xenaToMatrix.py
+++ support/xenaToMatrix.py
@@ -1,60 +1,90 @@
 import sys,string,os
 
-non_silent_cutoff =10
+non_silent_cutoff = 1
 
 typeDic={
-    "Nonsense_Mutation":1,
-    "stop_gained":1,
-    "Frame_Shift_Del":2,
-    "Frame_Shift_Ins":2,
-    "frameshift_variant":2,
-    "Splice_Site":2,
-    "splice_acceptor_variant":4,
-    "splice_donor_variant":4,
-    'splice_acceptor_variant&intron_variant':4,
-    'splice_donor_variant&intron_variant':4,
-    
-    "splice_region_variant":4,
-    "splice_region_variant&intron_variant": 4,
-    "missense_variant":5,
-    "Missense_Mutation":5,
-    "missense":5,
-    "Nonstop_Mutation":6,
-    "stop_lost":6,
-    "start_lost":6,
-    "start_gained":6,
-    "initiator_codon_variant":6,
-    "De_novo_Start_OutOfFrame":6,
-    "Translation_Start_Site":6,
-    "De_novo_Start_InFrame":6,
-    "5_prime_UTR_premature_start_codon_gain_variant":6,
-    "disruptive_inframe_deletion":7,
-    "disruptive_inframe_insertion":7,
-    "In_Frame_Del":7,
-    "inframe_deletion":7,
-    "In_Frame_Ins":8, 
-    "inframe_insertion":8,
-    "Indel":8,
-    "RNA":9,
-    "non_coding_exon_variant": 9,
+    'Nonsense_Mutation': 4,
+    'Nonsense': 4,
+    'frameshift_variant': 4,
+    'Frameshift': 4,
+    'stop_gained': 4,
+    'Stop Gained': 4,
+    'Frame_Shift_Del': 4,
+    'Frame_Shift_Ins': 4,
+    'Frameshift Deletion': 4,
+    'Frameshift Insertion': 4,
 
-    "Silent":100 ,##
-    "synonymous_variant":100, ##
-    "stop_retained_variant":100, ##
-    "3_prime_UTR_variant":100, ##
-    "5_prime_UTR_variant":100, ##
-    "5'Flank":100,##
-    "3'Flank":100,##
-    "5'UTR":100,##
-    "3'UTR":100,##
-    "downstream_gene_variant":100, ##
-    "upstream_gene_variant":100,##
-    "exon_variant":100, ##
-    "intergenic_region":100, ##
-    "intron_variant": 100, ##
-    "Intron":100, ##
-    "IGR":100 ##
-    }
+    #splice related
+    'splice_acceptor_variant': 3,
+    'splice_acceptor_variant&intron_variant': 3,
+    'splice_donor_variant': 3,
+    'splice_donor_variant&intron_variant': 3,
+    'SpliceAcceptorDeletion': 3,
+    'SpliceAcceptorSNV': 3,
+    'SpliceDonorBlockSubstitution': 3,
+    'SpliceDonorDeletion': 3,
+    'SpliceDonorSNV': 3,
+    'Splice_Site': 3,
+    'splice_region_variant': 3,
+    'splice_region_variant&intron_variant': 3,
+
+    #modify protein
+    'missense': 2,
+    'non_coding_exon_variant': 2,
+    'missense_variant': 2,
+    'Missense Variant': 2,
+    'Missense_Mutation': 2,
+    'Missense': 2,
+    'MultiAAMissense': 2,
+    'start_lost': 2,
+    'start_gained': 2,
+    'De_novo_Start_OutOfFrame': 2,
+    'Translation_Start_Site': 2,
+    'CdsStartSNV': 2,
+    'De_novo_Start_InFrame': 2,
+    'stop_lost': 2,
+    'Stop Lost': 2,
+    'Nonstop_Mutation': 2,
+    'initiator_codon_variant': 2,
+    '5_prime_UTR_premature_start_codon_gain_variant': 2,
+    'disruptive_inframe_deletion': 2,
+    'disruptive_inframe_insertion': 2,
+    'inframe_deletion': 2,
+    'Inframe Deletion': 2,
+    'InFrameDeletion': 2,
+    'inframe_insertion': 2,
+    'Inframe Insertion': 2,
+    'InFrameInsertion': 2,
+    'In_Frame_Del': 2,
+    'In_Frame_Ins': 2,
+    'Indel': 2,
+
+    #do not modify protein
+    'synonymous_variant': 1,
+    'Synonymous Variant': 1,
+    'Synonymous': 1,
+    'Silent': 1,
+    'stop_retained_variant': 1,
+
+    #mutations effect we don't know
+    'lincRNA': 0,
+    'RNA': 0,
+    'exon_variant': 0,
+    'upstream_gene_variant': 0,
+    'downstream_gene_variant': 0,
+    "5'Flank": 0,
+    "3'Flank": 0,
+    "3'UTR": 0,
+    "5'UTR": 0,
+    '5_prime_UTR_variant': 0,
+    '3_prime_UTR_variant': 0,
+    'Complex Substitution': 0,
+    'intron_variant': 0,
+    'intergenic_region': 0,
+    'intron': 0,
+    'Intron': 0,
+    "IGR":0
+}
 
 
 #xena data file
@@ -108,7 +138,7 @@ def process (file, samples, genes, dic):
         if sample not in dic[gene]:
             dic[gene][sample]=mtype
         else:
-            if mtype < dic[gene][sample]:
+            if mtype > dic[gene][sample]:
                 dic[gene][sample]=mtype
     fin.close()
 
@@ -120,7 +150,7 @@ def output (outfile, samples, genes, dic):
         for sample in samples:
             try:
                 mtype = dic[gene][sample]
-                if mtype < non_silent_cutoff:
+                if mtype > non_silent_cutoff:
                     fout.write("\t1")
                 else:
                     fout.write("\t0")
