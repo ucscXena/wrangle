187c00bd6a176ede4eb911894c3122b00327bb20
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2017-01-19 14:35:03 -0800

    edit

--- support/xenaIDreplaceGenomicMatrix.py
+++ support/xenaIDreplaceGenomicMatrix.py
@@ -19,41 +19,89 @@ def mapping (mfile):
     fin.close()
     return mapDic
 
-def switchFirstlineIDs (inputFile, outputFile, mapDic):
+def switchFirstlineIDs (inputFile, outputFile, mapDic, strict_inclusion = False, exclusion_mapDic = {}):
     fin = open(inputFile,'U')
     fout = open(outputFile,'w')
-    
     ids = string.split(fin.readline()[:-1],'\t')
     newIds =[ids[0]]
-    for id in ids[1:]:
+    exclusion_pos = []
+    inclusion_pos = [] #strict situation
+
+    for i in range (1, len(ids)):
+        id = ids[i]
         if not id in mapDic:
-            print id
+            print id, "not found"
             xenaID = id
         else:
             xenaID = mapDic[id]
+            if strict_inclusion:
+                inclusion_pos.append(i)
+
+            if xenaID in newIds:
+                print id, xenaID, "duplicate"
+
+        if id in exclusion_mapDic:
+            print id, "skip"
+            exclusion_pos.append(i)
+            
         newIds.append(xenaID)
-    fout.write(string.join(newIds,'\t')+'\n')
+
+    if len(exclusion_pos) == 0 and len(inclusion_pos) == 0:
+        fout.write(string.join(newIds,'\t')+'\n')
+    else:
+        fout.write(newIds[0])
+        for i in range(1, len(newIds)):
+            if i not in exclusion_pos:
+                if not strict_inclusion or  i in inclusion_pos:
+                    fout.write('\t'+newIds[i])
+        fout.write('\n')
+
     while 1:
         line = fin.readline()
         if line =="":
             break
-        fout.write(line)
+        if len(exclusion_pos) == 0 and len(inclusion_pos) == 0:
+            fout.write(line)
+        else:
+            data = string.split(line[:-1],'\t')
+            fout.write(data[0])
+            for i in range(1, len(data)):
+                if i not in exclusion_pos:
+                    if not strict_inclusion or  i in inclusion_pos:
+                        fout.write('\t'+data[i])
+            fout.write('\n')
+
     fin.close()
     fout.close()
 
-if len(sys.argv[:])!=3:
+if len(sys.argv[:]) < 4 or  len(sys.argv[:]) > 5 :
     print 
-    print "python xenaIDreplaceGenomicMatrix.py matrixfile mappingfile(new current)"
+    print "python xenaIDreplaceGenomicMatrix.py matrixfile inclusion_mappingfile(new current) strict_inclusion(T/F) exclusion_mappping_file(optional)"
     print "mapping file: no header"
     print "              new_id old_id"
     print 
     sys.exit()
 
-mappingfile = sys.argv[2]
 inputfile = sys.argv[1]
+
+mappingfile = sys.argv[2]
+mapDic = mapping (mappingfile)
+
+strict_inclusion = sys.argv[3]
+if strict_inclusion not in ["T","F"]:
+    print "bad strict_inclusion value"
+    sys.exit()
+if strict_inclusion == "T":
+    strict_inclusion = True
+else:
+    strict_inclusion = False
+
+exclusion_mapDic = {}
+if len(sys.argv[:]) == 5:
+    exclusion_mappingfile = sys.argv[4]
+    exclusion_mapDic = mapping (exclusion_mappingfile)
 
 backupfile = copyOriginalFile(inputfile)
 
-mapDic = mapping (mappingfile)
-switchFirstlineIDs (backupfile, inputfile, mapDic)
+switchFirstlineIDs (backupfile, inputfile, mapDic, strict_inclusion, exclusion_mapDic)
 
