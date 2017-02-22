bd3da5113bfb34763ef7330a376cc2cd56998542
Jing <jingchunzhu@gmail.com>
2017-02-22 12:01:05 -0800

    edit

---
+++ support/xenaIDreplaceMutationVector_clinicalMatrix.py
@@ -0,0 +1,82 @@
+import string, sys,os
+
+def copyOriginalFile (filename):
+    os.system("cp " +filename +" "+filename+"_BK")
+    return filename+"_BK"
+
+def mapping (mfile):
+    fin = open(mfile,'U')
+    mapDic ={}
+    for line in fin.readlines():
+        data = string.split(line[:-1],'\t')
+        if len(data)==2:
+            xenaSampleID, sourceID = data
+            if sourceID not in mapDic:
+                mapDic [sourceID]=xenaSampleID
+            else:
+                print "ERROR", sourceID, "exists"
+    fin.close()
+    return mapDic
+
+def switchIDs (inputFile, outputFile, mapDic, strict_inclusion = False, exclusion_mapDic = {}):
+    fin = open(inputFile,'U')
+    fout = open(outputFile,'w')
+
+    #header
+    fout.write(fin.readline())
+
+    #data
+    while 1:
+        line = fin.readline()
+        if line =="":
+            break
+        data = string.split(line[:-1],'\t')
+        id = data[0]
+        if id not in mapDic:
+            newID = id
+            if strict_inclusion:
+                print id, "not found skip"
+                continue
+            else:
+                print id, "not found keep"
+        else:
+            newID = mapDic[id]
+
+        if id in exclusion_mapDic:
+            print id, "skip"
+            continue
+
+        data[0]=newID
+        fout.write(string.join(data,'\t')+'\n')
+    fin.close()
+    fout.close()
+
+if len(sys.argv[:]) < 4 or  len(sys.argv[:]) > 5 :
+    print
+    print "xenaIDreplaceMutationVector_clinicalMatrix.py mutationVector_clinMatrix_file(with_Header) inclusion_mappingfile(new current) strict_inclusion(T/F) exclusion_mappping_file(optional)"
+    print "mapping file: no header"
+    print "              new_id old_id"
+    print
+    sys.exit()
+
+inputfile = sys.argv[1]
+
+mappingfile = sys.argv[2]
+mapDic = mapping (mappingfile)
+
+strict_inclusion = sys.argv[3]
+if strict_inclusion not in ["T","F"]:
+    print "bad strict_inclusion value"
+    sys.exit()
+if strict_inclusion == "T":
+    strict_inclusion = True
+else:
+    strict_inclusion = False
+
+exclusion_mapDic = {}
+if len(sys.argv[:]) == 5:
+    exclusion_mappingfile = sys.argv[4]
+    exclusion_mapDic = mapping (exclusion_mappingfile)
+
+backupfile = copyOriginalFile(inputfile)
+switchIDs (backupfile, inputfile, mapDic, strict_inclusion, exclusion_mapDic)
