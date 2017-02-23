57ee423c187097c2dd0a54b04e2b0676a017b201
Jing <jingchunzhu@gmail.com>
2017-02-23 11:54:14 -0800

    edit

--- support/keepIDClinicalMatrixMutationVector.py
+++ support/keepIDClinicalMatrixMutationVector.py
@@ -4,7 +4,7 @@ def listing (mfile):
     fin = open(mfile,'r')
     list =[]
     for line in fin.readlines():
-        data = string.strip(line)
+        data = string.strip(string.split(line,'\t')[0])
         if data not in list:
             list.append(data)
     fin.close()
@@ -24,7 +24,7 @@ def keepIDs (inputFile, outputFile, keep_list):
     fout.close()
 
 if len(sys.argv[:])!=4:
-    print "keepIDClinicalMatrix.py matrixfile output keep_list(one_id_per_line)"
+    print "keepIDClinicalMatrix.py matrixfile output keep_list(first_column_id)"
     sys.exit()
 
 listfile = sys.argv[3]
