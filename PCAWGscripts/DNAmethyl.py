c786e03ef4a3ad74cfa0d6026090d0b6b4a42b80
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2017-02-24 12:58:15 -0800

    edit

--- PCAWGscripts/DNAmethyl.py
+++ PCAWGscripts/DNAmethyl.py
@@ -1,12 +1,24 @@
-import os, sys
+import os, sys, string
 
 def process (inputdir):
-    for file in os.path.listdir(inputdir):
-        print file
+    for file in os.listdir(inputdir):
+        if (not os.path.isfile(file)):
+            continue
+        input = inputdir + "/" + file
+        sample = string.replace(file,'.txt','')
+
+        fout = open("new",'w')
+        fout.write("sample\t"+ sample+'\n')
+        fout.close()
+        os.system("cat "+ input + " >> new") 
+        print sample
+        os.system("mv new "+ inputdir + "/" + sample +".xena")
 
 if len(sys.argv[:]) != 2:
     print "python DNAmethyl.py inputdir"
     sys.exit()
 
 inputdir = sys.argv[1]
+if inputdir[-1]=='/':
+    inputdir = inputdir[:-1]
 process (inputdir)
