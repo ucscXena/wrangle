833d98958099b2d5c997eb63130ff450d3c8a87a
Jing <jingchunzhu@gmail.com>
2017-02-24 12:58:11 -0800

    add

---
+++ PCAWGscripts/DNAmethyl.py
@@ -0,0 +1,12 @@
+import os, sys
+
+def process (inputdir):
+    for file in os.path.listdir(inputdir):
+        print file
+
+if len(sys.argv[:]) != 1:
+    print "python DNAmethyl.py inputdir"
+    sys.exit()
+
+inputdir = sys.argv[1]
+process (inputdir)
