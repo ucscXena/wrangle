57dd6eddf2caa51b0c0cd2d2e07ce9667a8872ef
Jing <jingchunzhu@gmail.com>
2017-03-30 16:21:34 -0700

    edit

--- Nof1Analysis/itomic_Nof1_withITOMIC.py
+++ Nof1Analysis/itomic_Nof1_withITOMIC.py
@@ -23,7 +23,7 @@ def Nof1_output (Nof1_sample, original_label, gene, itomic_Data, Nof1_theta):
 
     print "log2(TPM):", Nof1_value, "TPM:", '{:.2f}'.format(Nof1_TPM)
 
-def filer_header (comparison_list, itomic_samples, Nof1_item, fout):
+def filer_header (comparison_list, itomic_samples, Nof1_item, Nof1_sample, fout):
     headerList =["label","gene"]
     header2ndList =["",""]
     headerList.append(Nof1_item["label"]+ ' (n=' + str(len(itomic_samples))+")")
@@ -42,7 +42,6 @@ def filer_header (comparison_list, itomic_samples, Nof1_item, fout):
         header2ndList.append("mean itomic")
         header2ndList.append("mean " + item["label"])
 
-    Nof1_sample = Nof1_item["samples"][0]
     headerList.extend([Nof1_sample, Nof1_sample])
     header2ndList.extend(["log2(TPM)","TPM"])
 
@@ -58,7 +57,7 @@ def itomic_Nof1(Nof1_sample, Nof1_item, original_labels, geneMappping, external_
 
     #file header output
     fout = open(outputfile,'w')
-    filer_header (external_comparison_list, itomic_samples, Nof1_item, fout)
+    filer_header (external_comparison_list, itomic_samples, Nof1_item, Nof1_sample, fout)
 
     pDic = {} # pvalue collection
     for original_label in original_labels:
