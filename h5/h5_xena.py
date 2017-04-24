4158c61b26f0dca8d397dc5c2b9a108f02b238a2
Jing <jingchunzhu@gmail.com>
2017-04-24 14:35:13 -0700

    edit

--- h5/h5_xena.py
+++ h5/h5_xena.py
@@ -1,14 +1,7 @@
 import h5py
-import string, sys
+import string
 import numpy as np
-
-def print_attrs(name, obj):
-    print name, len(obj)
-
-def get_h5_info (h5_file):
-    hF = h5py.File(h5_file)
-    print hF.keys()
-    hF.visititems(print_attrs)
+import sys
 
 #output to mtx format -- still need to add header lines
 def output_to_mtx (output, colN, indices, data):
@@ -75,19 +68,18 @@ def h5_T_to_xena (output, data, indices, indptr, counter_indptr_size, genes, bar
         fout.write(gene+'\t'+string.join(map(lambda x: str(x), values),'\t')+'\n')
     fout.close()
 
-
-def h5_to_xena (output, data, indices, indptr, counter_indptr_size, genes, barcodes):
+# [start:end)
+def h5_to_xena (output, data, indices, indptr, counter_indptr_size, genes, barcodes, start, end):
     fout =open(output,'w')
-    fout.write("sample\t"+string.join(barcodes,'\t')+'\n')
+    if start ==0:
+        fout.write("sample\t"+string.join(barcodes,'\t')+'\n')
 
     #the standard CSC representation
     #where the row indices for column i are stored in indices[indptr[i]:indptr[i+1]] and
     #their corresponding values are stored in data[indptr[i]:indptr[i+1]].
     #If the shape parameter is not supplied, the matrix dimensions are inferred from the index arrays.
 
-    N = len(indptr) -1 ### ?
-
-    for i in range (0, N):
+    for i in range (start, end):
         gene = genes[i]
         indices_range = indices[indptr[i]:indptr[i+1]]
         data_range = data[indptr[i]:indptr[i+1]]
@@ -96,22 +88,16 @@ def h5_to_xena (output, data, indices, indptr, counter_indptr_size, genes, barco
             index = indices_range[j]
             value = data_range[j]
             values[index] = value
-        fout.write(gene+'\t')
-        print values
-        np.savetxt(fout, values[:counter_indptr_size-1], fmt='%i', delimiter='\t', newline='\t')
-        fout.write(str(values[-1])+'\n')
-        #fout.write(gene+'\t'+string.join(map(lambda x: str(x), values),'\t')+'\n')
+        fout.write(gene+'\t'+string.join(map(lambda x: str(x), values),'\t')+'\n')
     fout.close()
 
-
-if __name__ == "__main__" and len(sys.argv[:])!=4:
-    print "pyton h5_xena.py h5_input group_name tsv_output"
+if __name__ == "__main__" and len(sys.argv[:])!=6:
+    print "pyton h5_xena.py h5_input group_name tsv_output start_list(inclusive) end_list(exlusive)\n"
     sys.exit()
 
 matrix_h5 = sys.argv[1]
 output = sys.argv[3]
 group = sys.argv[2]
-get_h5_info (matrix_h5)
 
 hF = h5py.File(matrix_h5)
 indptr = hF[group +"/indptr"]
@@ -123,15 +109,14 @@ barcodes = hF[group + "/barcodes"]
 shape = hF[group + "/shape"]
 rowN = shape[0]
 colN = shape[1]
-print "row", rowN
-print "col", colN
+
+start = int(sys.argv[4])
+end = int(sys.argv[5])
 
 assert(len(indptr) -1 == colN)
+assert (end < len(indptr))
 
 counter_indptr_size = rowN
-h5_to_xena (output, data, indices, indptr, counter_indptr_size, genes, barcodes)
-#h5_T_to_xena (output, data, indices, indptr, counter_indptr_size, genes, barcodes)
 
-
-
+h5_to_xena (output, data, indices, indptr, counter_indptr_size, genes, barcodes, start, end)
 
