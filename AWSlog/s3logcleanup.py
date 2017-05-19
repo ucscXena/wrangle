26fdf724cbaa423e27d01adabaa0613afd152b5e
jingchunzhu <jingchunzhu@gmail.com>
2017-05-19 07:33:04 -0700

    edit

--- AWSlog/s3logcleanup.py
+++ AWSlog/s3logcleanup.py
@@ -1,12 +1,27 @@
 import string, os, sys
 import dateutil.parser
 
+#unify two types of load balancer logs
+#http://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-log-entry-format
+#http://docs.aws.amazon.com/elasticloadbalancing/latest/classic/access-log-collection.html#access-log-entry-format
+def unifyLog(mergedlog, unifiedlog):
+	fin = open(mergedlog, 'r')
+	fout = open(unifiedlog, 'w')
+	for line in fin.readlines():
+		data = string.split(line, ' ')
+		if data[0] in ["http", "https", "h2", "ws", "wss"]:
+			fout.write(string.join(data[1:], ' '))
+		else:
+			fout.write(line)
+	fin.close()
+	fout.close()
+
 #remove extra lines in log file  (sorted) that are from the same ip for the same file download that are within 1 min, keep only one line
 def removeVeryCloseSameIPSameFile (logfile, cleanedlog):
 	# collect info
 	fin = open(logfile, 'r')
 	time_dic = {} #key = ip_target, values: timestamp
-
+	
 	for line in fin.readlines():
 		data = string.split(line, ' ')
 		try:
@@ -38,12 +53,12 @@ def removeVeryCloseSameIPSameFile (logfile, cleanedlog):
 			d1 = dateutil.parser.parse(time_list[i])
 			d2 = dateutil.parser.parse(time_list[i+1])
 			d = d2 - d1
-			if (d.total_seconds() < 60.0):
+			if (d.total_seconds() < 60.0): 
 				if time_list[i+1] not in bad_line_dic:
 					bad_line_dic[time_list[i+1]] = [key]
 				else:
 					bad_line_dic[time_list[i+1]].append(key)
-
+	
 	#remove bad lines
 	fin = open(logfile, 'r')
 	fout = open(cleanedlog, 'w')
@@ -63,13 +78,31 @@ def removeVeryCloseSameIPSameFile (logfile, cleanedlog):
 
 def process (indir):
 	for root, dirs, files in os.walk(indir):
+		for file in files:
+			filepath = root + '/'+ file
+			if filepath[-7:] == ".log.gz":
+				os.system("gunzip " + filepath)
+
+	for root, dirs, files in os.walk(indir):
 		for dir in dirs:
+			foundLog = 0
+			for filepath in os.listdir(root + '/' + dir):
+				if filepath[-4:] == ".log":
+					foundLog = 1
+			if foundLog == 0:
+				continue
+
 			dirpath = root+'/'+dir
-			mergedlog = root+'/'+dir + '/rawMerge'
-			cleanedlog = root+'/'+dir + '/cleanMerge'
-			print "clean", dirpath
-			os.system("cat " + dirpath + "/*.log | LC_ALL='C' sort > " + mergedlog)
-			removeVeryCloseSameIPSameFile (mergedlog, cleanedlog)
+			mergedlog = root+'/'+dir + '/rawMerge'  
+			unifiedlog = root+'/'+dir + '/unifiedMerge'  
+			sortedlog = root+'/'+dir + '/sortMerge'  
+			cleanedlog = root+'/'+dir + '/cleanMerge'  
+			print "cleaning", dirpath
+			os.system("cat " + dirpath + "/*.log > " + mergedlog)
+			unifyLog(mergedlog, unifiedlog)
+			os.system("LC_ALL='C' sort " + unifiedlog + " > " + sortedlog)
+			removeVeryCloseSameIPSameFile (sortedlog, cleanedlog)
+			os.system("rm " + string.join([mergedlog, unifiedlog, sortedlog], ' '))
 
 if len(sys.argv[:]) != 2 :
 	print "python s2logcleanup.py directory_name"
