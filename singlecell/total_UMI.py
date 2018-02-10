21a82e8b479bc381210018c5015ce54a4ff35349
EC2 Default User <ec2-user@ip-172-31-4-186.ec2.internal>
2018-02-10 08:31:05 +0000

    edit

--- singlecell/total_UMI.py
+++ singlecell/total_UMI.py
@@ -3,7 +3,21 @@ import string, sys, os
 def column_N(input):
 	fin = open(input,'r')
 	N = len(string.split(fin.readline(),'\t'))
+        fin.close()
 	return N
+
+def clinMatrixOut(input, output):
+        fin = open(input,'r')
+        fout = open(output,'w')
+        fout.write("cell\ttotal_UMI_counts\n")
+        cells = string.split(fin.readline()[:-1],'\t')
+        values = string.split(fin.readline()[:-1],'\t')
+        for i in range (0, len(cells)):
+                cell = cells[i][4:-1]
+                value = values[i]
+                fout.write(cell +'\t'+ value+'\n')
+        fin.close()
+        fout.close()
 
 if len(sys.argv[:])!=3:
 	print "python total_UMI.py xena_matrix_input output"
@@ -11,10 +25,9 @@ if len(sys.argv[:])!=3:
 
 input = sys.argv[1]
 output = sys.argv[2]
-fout = open(output,'w')
-fout.close()
 
 colN = column_N(input)
+os.system("datamash -H sum 2-" + str(colN) + " < "+ input + "> .out")
 
-os.system("datamash -H sum 2-" + str(colN) + " < "+ input + ">>" + output)
+clinMatrixOut(".out", output)
 
