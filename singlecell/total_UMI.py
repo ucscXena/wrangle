38afe98313f07a1b42174ebd410f52658b27ca72
jingchunzhu <jingchunzhu@gmail.com>
2018-02-09 23:58:09 -0800

    edit

--- singlecell/total_UMI.py
+++ singlecell/total_UMI.py
@@ -6,7 +6,7 @@ def column_N(input):
 	return N
 
 if len(sys.argv[:])!=3:
-	print "python total_cellcolumn.py xena_matrix_input output"
+	print "python total_UMI.py xena_matrix_input output"
 	sys.exit()
 
 input = sys.argv[1]
@@ -15,12 +15,6 @@ fout = open(output,'w')
 fout.close()
 
 colN = column_N(input)
-K = 1000
 
 os.system("datamash -H sum 2-" + str(colN) + " < "+ input + ">>" + output)
 
-"""
-for i in range(1, colN, K):
-        print i
-	os.system("datamash -H sum "+ str(i+1) +"-" + str(i+K-1) + "< "+ input + ">>" + output)
-"""
