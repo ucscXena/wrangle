5697ad1f1861cf371b910f76d5e2ee6e85cccb8f
jingchunzhu <jingchunzhu@gmail.com>
2018-02-16 17:15:26 -0800

    edit

--- singlecell/bh_tsne_ready.py
+++ singlecell/bh_tsne_ready.py
@@ -2,11 +2,10 @@ import string, sys, os
 
 def bh_tsne_ready(input, output):
 	os.system("head -n 1 " + input + " | tr '\t' '\n' | tail -n +2 > bh_tsne_cells")
-
 	os.system("tail -n +2 " + input + " | cut -f 2- > .bh_tsne_T")
-	
 	dir = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
 	os.system("python "+ dir +"/support/transpose.py .bh_tsne_T " + output)
+	os.system("rm .bh_tsne_T")
 
 if len(sys.argv[:])!= 3:
 	print "python bh_tsne_ready.py input_matrix output_bhtsne(transpose no headers)"
