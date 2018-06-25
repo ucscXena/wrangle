0e16a99639c22a1240c3f780e8478fb96680cb4b
jingchunzhu <jingchunzhu@gmail.com>
2018-06-25 16:43:02 -0700

    edit

---
+++ ATACseqscripts/buildEnhancer.py
@@ -0,0 +1,40 @@
+import string, sys
+
+enhancerlinkageFile = "ATAC_Peak_Linkage.txt"
+probeFile = "TCGA_ATAC_peak.probeMap"
+newProbeMap = "TCGA_ATAC_peak.Linkage.probeMap"
+
+def processLowQprobeMap(probeFile):
+	fin = open(probeFile,'U')
+	fin.readline()
+	probeMapDic ={} #key: chr_start_end value: probeID
+	for line in fin.readlines():
+		data = string.split(line[:-1], '\t')
+		peakID = data[0]
+		chrom = data[2]
+		start = data[3]
+		end = data[4]
+		probeMapDic[string.join([chrom, start, end],'_')] = peakID
+	fin.close()
+	return probeMapDic
+
+def processLinkagefile(enhancerlinkageFile, probeMapDic):
+	fin = open(enhancerlinkageFile,'U')
+	fout = open(newProbeMap, 'w')
+	fin.readline()
+	fout.write("id\tgene\tchrom\tchromStart\tchromEnd\tstrand")
+	for line in fin.readlines():
+		data = string.split(line[:-1], '\t')
+		enhancerID = data[-1]
+		chrom = data[0]
+		start = str(int(float(data[1])))
+		end = str(int(float(data[2])))
+		targetGene = data[6]
+		coord_id = string.join([chrom, start, end],'_')
+		peakID = probeMapDic[coord_id]
+		fout.write(string.join([peakID, targetGene, chrom, start, end, '+'],'\t') + '\n')
+	fin.close()
+	fout.close()
+
+probeMapDic = processLowQprobeMap(probeFile)
+processLinkagefile(enhancerlinkageFile, probeMapDic)
