952878e72a1e453f785c10eb308397763c92dd96
jingchunzhu <jingchunzhu@gmail.com>
2018-07-02 11:42:38 -0700

    add elmer scripts

---
+++ Elmerscripts/buildElmerProbeMap.py
@@ -0,0 +1,51 @@
+import string, sys
+
+illuminProbemapfile ="illuminaMethyl450_hg38_GDC"
+elemerfile = "getPair.hypo.pairs.significant.csv"
+outputProbeMap= "elmer.hypo.pairs.significant.probeMap"
+
+def probeMapParse(illuminProbemapfile):
+	probeDic ={}
+	fin = open(illuminProbemapfile, 'U')
+	fin.readline()
+	while 1:
+		line = fin.readline()
+		if line == '':
+			break
+		data = string.split(line[:-1],'\t')
+		id = data[0]
+		chrom = data[2]
+		start = data[3]
+		end = data[4]
+		probeDic[id] = [chrom, start, end]
+	fin.close()
+	return probeDic
+
+def elmerFileParse (elemerfile):
+	elemerGeneDic ={}
+	fin = open(elemerfile, 'U')
+	fin.readline()
+	for line in fin.readlines():
+		data= string.split(line[:-1],',')
+		id = data[0]
+		gene =data[2]
+		if id not in elemerGeneDic:
+			elemerGeneDic[id]=[]
+		if gene not in elemerGeneDic[id]:
+			elemerGeneDic[id].append(gene)
+	fin.close()
+	return elemerGeneDic
+
+def buildElmerProbeMap(probeDic, elemerGeneDic,outputProbeMap):
+	fout = open(outputProbeMap, 'w')
+	fout.write("id\tgene\tchrom\tchromStart\tchromEnd\tstrand\n")
+	for id in elemerGeneDic:
+		genes = string.join(elemerGeneDic[id],',')
+		chrom, start, end = probeDic[id]
+		fout.write(string.join([id, genes, chrom, start, end, '.'],'\t')+'\n')
+	fout.close()
+
+probeDic = probeMapParse(illuminProbemapfile)
+elemerGeneDic = elmerFileParse (elemerfile)
+
+buildElmerProbeMap(probeDic, elemerGeneDic,outputProbeMap)
