4454aa550d8533f482563357d89c16c2be6cc3df
jingchunzhu <jingchunzhu@gmail.com>
2018-07-05 00:36:21 -0700

    edit

--- ATACseqscripts/buildEnhancerProbeMap.py
+++ ATACseqscripts/buildEnhancerProbeMap.py
@@ -1,14 +1,14 @@
 import string, sys
 
 #### gene enhancer linkage for pan-can
-# enhancerlinkageFile = "ATAC_Peak_Linkage.txt"
-# probeFile = "TCGA_ATAC_peak.probeMap"
-# newProbeMap = "TCGA_ATAC_peak.Linkage.probeMap"
+enhancerlinkageFile = "ATAC_Peak_Linkage.txt"
+probeFile = "TCGA_ATAC_peak.probeMap"
+newProbeMap = "TCGA_ATAC_peak.Linkage.probeMap"
 
 #### gene enhancer linkage for brca
-enhancerlinkageFile = "BRCA_ATAC_Peak_Linage.txt"
-probeFile = "brca_peak.probeMap"
-newProbeMap = "brca_enhencer.probeMap"
+#enhancerlinkageFile = "BRCA_ATAC_Peak_Linage.txt"
+#probeFile = "brca_peak.probeMap"
+#newProbeMap = "brca_enhencer.probeMap"
 
 def processLowQprobeMap(probeFile):
 	fin = open(probeFile,'U')
@@ -26,12 +26,10 @@ def processLowQprobeMap(probeFile):
 
 def processLinkagefile(enhancerlinkageFile, probeMapDic):
 	fin = open(enhancerlinkageFile,'U')
-	fout = open(newProbeMap, 'w')
 	fin.readline()
-	fout.write("id\tgene\tchrom\tchromStart\tchromEnd\tstrand\n")
+	newProbeMapDic = {}
 	for line in fin.readlines():
 		data = string.split(line[:-1], '\t')
-		enhancerID = data[-1]
 		chrom = data[0]
 		start = str(int(float(data[1])))
 		end = str(int(float(data[2])))
@@ -39,10 +37,22 @@ def processLinkagefile(enhancerlinkageFile, probeMapDic):
 		coord_id = string.join([chrom, start, end],'_')
 		try:
 			peakID = probeMapDic[coord_id]
-			fout.write(string.join([peakID, targetGene, chrom, start, end, '+'],'\t') + '\n')
+			if peakID not in newProbeMapDic:
+				newProbeMapDic[peakID] = [[targetGene], chrom, start, end]
+			else:
+				if targetGene not in newProbeMapDic[peakID][0]:
+					newProbeMapDic[peakID][0].append(targetGene)
 		except KeyError:
 			print coord_id
+			continue
+
 	fin.close()
+
+	fout = open(newProbeMap, 'w')
+	fout.write("id\tgene\tchrom\tchromStart\tchromEnd\tstrand\n")
+	for peakID in newProbeMapDic:
+		targetGeneS, chrom, start, end = newProbeMapDic[peakID]
+		fout.write(string.join([peakID, string.join(targetGeneS, ','), chrom, start, end, '+'],'\t') + '\n')
 	fout.close()
 
 probeMapDic = processLowQprobeMap(probeFile)
