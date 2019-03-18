86411da8a6c60039ab340809f4681c56ec00e6a7
jingchunzhu <jingchunzhu@gmail.com>
2019-03-18 13:57:22 -0700

    edit

--- probeMap/ensemble.gene.transcript.probemap.bash
+++ probeMap/ensemble.gene.transcript.probemap.bash
@@ -1,4 +1,9 @@
-#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "usage: ./ensemble.gene.transcript.probemap.bash ensemble_downloaded_gtf"
+    echo
+    exit
+fi
+
 echo $1
 
 file=$1
@@ -14,8 +19,13 @@ awk '$3 == "gene"' $file > $genefile
 awk '$3 == "transcript"' $file > $transcriptfile
 
 echo -e 'id\tgene\tchrom\tchromStart\tchromEnd\tstrand' > $geneProbemap
-cut -f 1,4,5,7,9 $genefile |awk 'BEGIN{FS="\t"; OFS="\t"}{split($5,a,";");split(a[1],b," ");split(a[3],c," ");print substr(b[2],2,length(b[2])-2),substr(c[2],2, length(c[2])-2),$1,$2,$3,$4}' >> $geneProbemap
+cut -f 1,4,5,7,9 $genefile | \
+awk 'BEGIN{FS="\t"; OFS="\t"} 
+	{split($5,a,";");split(a[1],b," ");split(a[3],c," "); split(a[2],d," "); 
+	print substr(b[2],2,length(b[2])-2) "." substr(d[2],2,length(d[2])-2), substr(c[2],2, length(c[2])-2),$1,$2,$3,$4}' >> $geneProbemap
 
 echo -e 'id\tgene\tchrom\tchromStart\tchromEnd\tstrand' > $transcriptProbemap
-cut -f 1,4,5,7,9 $transcriptfile |awk 'BEGIN{FS="\t"; OFS="\t"}{split($5,a,";");split(a[3],b," ");split(a[5],c," ");print substr(b[2],2,length(b[2])-2),substr(c[2],2, length(c[2])-2),$1,$2,$3,$4}' >> $transcriptProbemap
-
+cut -f 1,4,5,7,9 $transcriptfile | \
+awk 'BEGIN{FS="\t"; OFS="\t"} 
+	{split($5,a,";");split(a[3],b," ");split(a[5],c," "); split(a[4],d," "); 
+	print substr(b[2],2,length(b[2])-2) "." substr(d[2],2,length(d[2])-2), substr(c[2],2, length(c[2])-2),$1,$2,$3,$4}' >> $transcriptProbemap
