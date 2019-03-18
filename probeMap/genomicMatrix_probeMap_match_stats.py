a8aeac8526098d9f2e1ddae240006e7c8a5c3445
jingchunzhu <jingchunzhu@gmail.com>
2019-03-18 16:36:33 -0700

    add

---
+++ probeMap/genomicMatrix_probeMap_match_stats.py
@@ -0,0 +1,32 @@
+import string, sys
+from sets import Set
+
+if len(sys.argv[:]) != 3:
+	print "python genomicMatrix_probeMap_match_stats.py genomicMatrix probeMap"
+	print
+	sys.exit()
+
+def parse(genomicMatrix_OR_probemap_file):
+	fin = open(genomicMatrix_OR_probemap_file, 'r')
+	fin.readline()
+
+	identifiers = []
+	while 1:
+		line = fin.readline()
+		if line == '':
+			break
+		data = string.split(line, '\t')
+		probe = data[0]
+		identifiers.append(probe)
+	fin.close()
+
+	return identifiers
+
+genomicMatrix_file = sys.argv[1]
+probeMap_file = sys.argv[2]
+
+probeMap_ids = Set(parse(probeMap_file))
+gMX_identifiers = Set(parse(genomicMatrix_file))
+
+print "probeMap id:", len(probeMap_ids)
+print "genomicMatrix id:", len(gMX_identifiers)
