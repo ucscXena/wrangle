40571d8e5112d755764b3ddeb265a23811b72e51
jingchunzhu <jingchunzhu@gmail.com>
2019-09-27 02:19:55 -0700

    edit

--- HCA/csv_run.py
+++ HCA/csv_run.py
@@ -10,15 +10,42 @@ def parse(configfile):
 	return dic
 
 if len(sys.argv[:])!= 2:
-        print ("python csv_run.py dataDir")
-        sys.exit()
+	print ("python csv_run.py dataDir")
+	sys.exit()
     
 dir = sys.argv[1]
 configfile = dir + '/config'
 
 if not os.path.exists(configfile):
-        print ("not config file")
- 	sys.exit()
+	print ("not config file")
+	sys.exit()
 
 metaDic = parse(configfile)
-print (metaDic)
+if "version" in metaDic:
+	version = metaDic["version"]
+else:
+	print ("missing version")
+	sys.exit()
+
+if "cohort" in metaDic:
+	cohort = metaDic["cohort"]
+else:
+	print ("missing cohort")
+	sys.exit()
+
+if "url" in metaDic:
+	version = metaDic["url"]
+else:
+	print ("missing url")
+	sys.exit()
+
+if "size" in metaDic:
+	version = metaDic["size"]
+else:
+	print ("missing size")
+	sys.exit()
+
+os.system("python cells.csv.json.py " + dir + " " + version + " " + cohort + " " + url)
+os.system("python expression.csv.json.py " + dir + " " + version + " " + cohort + " " + url)
+os.system("python cells.csv.py " + dir)
+os.system("python expression.csv.py " + dir + " " + size)
