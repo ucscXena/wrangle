a3d4dac55ebc3f98526b0acdee65f855bebba478
EC2 Default User <ec2-user@ip-172-31-8-218.ec2.internal>
2019-09-27 09:38:03 +0000

    edit

--- HCA/csv_run.py
+++ HCA/csv_run.py
@@ -46,9 +46,9 @@ else:
 	print ("missing size")
 	sys.exit()
 
-codedir = os.path.listdir(sys.argv[0])
-os.system("python " + codedir + "/cells.csv.json.py " + dir + " " + version + " " + cohort + " " + url)
-os.system("python " + codedir + "/expression.csv.json.py " + dir + " " + version + " " + cohort + " " + url)
+codedir = os.path.dirname(sys.argv[0])
+os.system("python " + codedir + "/cells.csv.json.py " + dir + " " + version + " \"" + cohort + "\" " + url)
+os.system("python " + codedir + "/expression.csv.json.py " + dir + " " + version + " \"" + cohort + "\" " + url)
 os.system("python " + codedir + "/cells.csv.py " + dir)
 if (expressionRun):
 	os.system("python " + codedir + "/expression.csv.py " + dir + " " + size)
