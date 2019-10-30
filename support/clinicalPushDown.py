5208c24d2332e8b1dc8450a60f00131c2f5825e4
jingchunzhu <jingchunzhu@gmail.com>
2019-10-30 16:54:01 -0700

    edit

--- support/clinicalPushDown.py
+++ support/clinicalPushDown.py
@@ -1,10 +1,10 @@
-import string,os,sys
+import string, os, sys
 
 def mapping (mfile):
-    fin = open(mfile,'U')
+    fin = open(mfile,'r')
     mapDic ={}
     for line in fin.readlines():
-        data = string.split(line[:-1],'\t')
+        data = line[:-1].split('\t')
         if len(data)==2:
             xenaSampleID, sourceID = data
             if sourceID not in mapDic:
@@ -16,26 +16,26 @@ def mapping (mfile):
     return mapDic
 
 def pushDown (inputFile, outputFile, mapDic, _PATIENT_field_header):
-    fin = open(inputFile, 'U')
+    fin = open(inputFile, 'r')
     fout = open(outputFile, 'w')
 
     #header
-    data = string.split(fin.readline(),'\t')
+    data = fin.readline().split('\t')
     N= len(data)
     fout.write("xena_sample\t" + _PATIENT_field_header + "\t")
-    fout.write(string.join(data[1:],'\t'))
+    fout.write('\t'.join(data[1:]))
 
     #data
     for line in fin.readlines():
-        data = string.split(line,'\t')
+        data = line.split('\t')
         if (N!= len(data)):
-            print data, len(data)
+            print (data, len(data))
         patientID = data[0]
         if patientID in mapDic:
             for sample in mapDic[patientID]:
                 donor = data[0]
                 fout.write(sample+'\t'+donor+'\t')
-                fout.write(string.join(data[1:],'\t'))
+                fout.write('\t'.join(data[1:]))
                 if (data[-1][-1]!='\n'):
                     fout.write('\n')
 
@@ -43,7 +43,7 @@ def pushDown (inputFile, outputFile, mapDic, _PATIENT_field_header):
     fout.close()
 
 if len(sys.argv[:]) not in  [4, 5]:
-    print "python clinicalPushDown.py patientLevelFile mapping(sample patient no_header) sampleLevelFile _PATIENT_field_header(optional)"
+    print ("python clinicalPushDown.py patientLevelFile mapping(sample patient no_header) sampleLevelFile _PATIENT_field_header(optional)")
     print
     sys.exit()
 
