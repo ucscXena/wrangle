62281852db976625b3ef8031c465c626725cb553
jingchunzhu <jingchunzhu@gmail.com>
2020-01-08 10:27:09 -0800

    edit

--- support/keepIDGenomicMatrix.py
+++ support/keepIDGenomicMatrix.py
@@ -1,10 +1,11 @@
-import string, sys,os
+import sys, os
 
 def listing (mfile):
     fin = open(mfile,'r')
     list =[]
     for line in fin.readlines():
-        data = string.strip(string.split(line,'\t')[0])
+        # data = string.strip(string.split(line,'\t')[0])
+        data = line.split('\t')[0].strip()
         if data not in list:
             list.append(data)
     fin.close()
@@ -14,7 +15,8 @@ def keepIDs (inputFile, outputFile, keep_list):
     fin = open(inputFile,'U')
 
     goodPos=[]
-    ids = string.split(fin.readline()[:-1],'\t')
+    #   ids = string.split(fin.readline()[:-1],'\t')
+    ids = fin.readline()[:-1].split('\t')
     for i in range (0, len(ids)):
         if ids[i] in keep_list:
             goodPos.append(i)
@@ -24,13 +26,13 @@ def keepIDs (inputFile, outputFile, keep_list):
     goodPos.sort()
     s= "cut -f 1,"  #pos=0 always in
     for i in goodPos[:-1]:
-        s= s+str(1+i)+","
-    s = s + str(1+goodPos[-1]) + " " +inputfile +" > " + outputFile
+        s = s + str(1+i) + ","
+    s = s + str(1 + goodPos[-1]) + " " + inputfile + " > " + outputFile
     os.system(s)
 
 if len(sys.argv[:])!=4:
-    print "python keepIDGenomicMatrix.py matrixfile output keep_list(one_sample_id_per_line, first column is id)"
-    sys.exit()
+    print ("python keepIDGenomicMatrix.py matrixfile output keep_list(one_sample_id_per_line, first column is id)")
+    sys.exit(1)
 
 listfile = sys.argv[3]
 keep_list  = listing (listfile)
