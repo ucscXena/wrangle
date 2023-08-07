1d98bcc38e5014e96efc9923741b8736c35c406b
jzhu <jzhu@jzhus-MacBook-Pro.local>
2023-08-07 16:08:10 -0700

    change echo to printf

--- probeMap/ensemble.gene.transcript.probemap.bash
+++ probeMap/ensemble.gene.transcript.probemap.bash
@@ -7,24 +7,24 @@ fi
 echo $1
 
 file=$1
-root=$(basename $file .gtf)
+root=$(basename $file)
 
 genefile=$root.gene
 transcriptfile=$root.transcript
 
-geneProbemap=$root.gene.probemap
-transcriptProbemap=$root.transcript.probemap
+geneProbemap=$genefile.probemap
+transcriptProbemap=$transcriptfile.probemap
 
 awk '$3 == "gene"' $file > $genefile
 awk '$3 == "transcript"' $file > $transcriptfile
 
-echo -e 'id\tgene\tchrom\tchromStart\tchromEnd\tstrand' > $geneProbemap
+printf '%b\t%b\t%b\t%b\t%b\t%b\n' "id" "gene" "chrom" "chromStart" "chromEnd" "strand" > $geneProbemap
 cut -f 1,4,5,7,9 $genefile | \
 awk 'BEGIN{FS="\t"; OFS="\t"} 
 	{split($5,a,";");split(a[1],b," ");split(a[3],c," "); split(a[2],d," "); 
 	print substr(b[2],2,length(b[2])-2) "." substr(d[2],2,length(d[2])-2), substr(c[2],2, length(c[2])-2),$1,$2,$3,$4}' >> $geneProbemap
 
-echo -e 'id\tgene\tchrom\tchromStart\tchromEnd\tstrand' > $transcriptProbemap
+printf '%b\t%b\t%b\t%b\t%b\t%b\n' "id" "gene" "chrom" "chromStart" "chromEnd" "strand" > $transcriptProbemap
 cut -f 1,4,5,7,9 $transcriptfile | \
 awk 'BEGIN{FS="\t"; OFS="\t"} 
 	{split($5,a,";");split(a[3],b," ");split(a[5],c," "); split(a[4],d," "); 
