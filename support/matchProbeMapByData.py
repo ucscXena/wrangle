3b2a4b17d90d688ba6cfc82bc0db3aa8a5794994
jzhu <jzhu@jzhus-MacBook-Pro.local>
2023-12-15 11:21:41 -0800

    add matchProbeMapByData.py

---
+++ support/matchProbeMapByData.py
@@ -0,0 +1,49 @@
+import sys, os
+
+def matchProbeMapByData (matrixfile, probeMapfile):
+    #matrix features
+    features = {}
+    fin = open(matrixfile,'r')
+    while 1:
+        line = fin.readline()
+        if line == "":
+            break
+        data = line[:-1].split('\t')
+        feature = data[0]
+        features[f]=''
+    fin.close()
+    matrix_set = {features.keys()}
+    
+    #probeMap
+    features = {}
+    fprobe = open(probeMapfile,'U')
+    line = fprobe.readline()
+    while 1:
+        line = fprobe.readline()
+        if line =="":
+            break
+
+        data = line[:-1].split('\t')
+        feature = data[0]
+        features[f]=''
+    fprobe.close()
+    probemap_set = {features.keys()}
+
+    # overlap
+    overlap_set = set.intersection(matrix_set, probemap_set)
+
+    # stats
+    print ("overlap:", len(overlap_set))
+    print ("matrix:", len(matrix_set), len(overlap_set)/len(matrix_set) )
+    print ("probemap:", len(probemap_set), len(probemap_set)/len(matrix_set))
+
+
+if len(sys.argv[:])!=3:
+    print 
+    print ("python matchProbeMapByData.py genomicMatrix probeMap newProbeMap\n")
+    sys.exit()
+
+matrixfile = sys.argv[1]
+probeMapfile = sys.argv[2]
+
+mathProbeMapByData (matrixfile, probeMapfile)
